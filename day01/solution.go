34e038d4709555f089590d8ddd76bb971dd5309b
Turtel216 <papadimitrios216@gmail.com>
2024-12-01 23:19:13 +0200

    added day 1 solution

---
+++ day01/solution.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+	"strconv"
+	"strings"
+
+	"github.com/Turtel216/advent-of-code-2024/utils"
+)
+
+func main() {
+	lines := utils.ReadFromFile("./day01/input.txt")
+
+	var list1, list2 []int
+
+	// Populate the lists
+	for _, line := range lines {
+		parts := strings.Split(line, "   ")
+		num1, _ := strconv.Atoi(parts[0])
+		num2, _ := strconv.Atoi(parts[1])
+		list1 = append(list1, num1)
+		list2 = append(list2, num2)
+	}
+
+	// Sort both lists
+	sort.Ints(list1)
+	sort.Ints(list2)
+
+	// Part 1: Calculate the sum of absolute differences
+	result1 := 0
+	for i := 0; i < len(list1); i++ {
+		result1 += utils.AbsInt(list1[i] - list2[i])
+	}
+	fmt.Printf("result 1: %d\n", result1)
+
+	// Part 2: Calculate the weighted sum based on occurrences in list2
+	occurrences := countOccurrences(list2)
+	result2 := 0
+	for _, num := range list1 {
+		if count, found := occurrences[num]; found {
+			result2 += num * count
+		}
+	}
+	fmt.Printf("result 2: %d\n", result2)
+}
+
+// countOccurrences returns a map of numbers to their occurrence counts
+func countOccurrences(list []int) map[int]int {
+	countMap := make(map[int]int)
+	for _, num := range list {
+		countMap[num]++
+	}
+	return countMap
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Turtel216/advent-of-code-2024
+
+go 1.23.3
