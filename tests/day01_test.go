572df716d4032d9981abc9e063792699bc9081a7
Turtel216 <papadimitrios216@gmail.com>
2024-12-02 19:04:23 +0200

    updated day01 testing

--- tests/day01_test.go
+++ tests/day01_test.go
@@ -7,8 +7,14 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+// Answers that passed the test on the advent of code website for the given input
+const (
+	acceptedAnswer1 = 2285373
+	acceptedAnswer2 = 21142653
+)
+
 func TestDayOne(t *testing.T) {
 	result1, result2 := day01.Solve("../day01/input.txt")
-	assert.Equal(t, 2285373, result1, "Incorrect solution for first part")
-	assert.Equal(t, 21142653, result2, "Incorrect solution for second part")
+	assert.Equal(t, acceptedAnswer1, result1, "Incorrect solution for first part")
+	assert.Equal(t, acceptedAnswer2, result2, "Incorrect solution for second part")
 }
