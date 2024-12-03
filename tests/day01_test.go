12e95e19707dca14a6c4eabc1dd03ee4a323944f
Turtel216 <papadimitrios216@gmail.com>
2024-12-03 21:44:58 +0200

    added day02 part 1 tests

--- tests/day01_test.go
+++ tests/day01_test.go
@@ -9,12 +9,12 @@ import (
 
 // Answers that passed the test on the advent of code website for the given input
 const (
-	acceptedAnswer1 = 2285373
-	acceptedAnswer2 = 21142653
+	acceptedDay01Answer1 = 2285373
+	acceptedDay01Answer2 = 21142653
 )
 
 func TestDayOne(t *testing.T) {
 	result1, result2 := day01.Solve("../day01/input.txt")
-	assert.Equal(t, acceptedAnswer1, result1, "Incorrect solution for first part")
-	assert.Equal(t, acceptedAnswer2, result2, "Incorrect solution for second part")
+	assert.Equal(t, acceptedDay01Answer1, result1, "Incorrect solution for first part")
+	assert.Equal(t, acceptedDay01Answer2, result2, "Incorrect solution for second part")
 }
